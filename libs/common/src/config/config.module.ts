faf0666efd65faf05babf0864d533884a1d1ec1c
Sumit Singh <sumitsingh@chingari.io>
2024-07-22 15:10:36 +0530

    code update

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [NestConfigModule.forRoot()],
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      }),
+    }),
+  ],
 })
 export class ConfigModule {}
