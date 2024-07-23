d8cadd6950021b4c268e07dfc929245ffb1cdfd4
Sumit Singh <sumitsingh@chingari.io>
2024-07-23 15:43:50 +0530

    code update

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,11 @@
+import { LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [LoggerModule, UsersModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useLogger(app.get(Logger));
+
+  const config = new DocumentBuilder()
+    .setTitle('Sleepr Auth')
+    .setDescription('Sleepr Auth API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
