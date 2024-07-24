ad5c958cfd9ab07908bdd065fc8b4b6e0b703e6c
Sumit Singh <sumitsingh@chingari.io>
2024-07-24 16:42:47 +0530

    code update

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
 import { GetUserDto } from './dto/get-user.dto';
@@ -8,10 +8,20 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async createUser(body: CreateUserDto) {
+    await this.validateCreateUserDto(body);
     return this.usersRepository.create({
       ...body,
       password: bcrypt.hashSync(body.password, 10),
     });
+  }
+
+  async validateCreateUserDto(createUserDto: CreateUserDto) {
+    try {
+      await this.usersRepository.findOne({ email: createUserDto.email });
+    } catch (err) {
+      return;
+    }
+    throw new UnprocessableEntityException('Email already exists');
   }
 
   async verifyUser(email: string, password: string) {
