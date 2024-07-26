46006ef2eec760258742cb3b09ab4cf8c29c5224
Sumit Singh <sumitsingh@chingari.io>
2024-07-26 16:18:22 +0530

    code update

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,10 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import nodemailer from 'nodemailer';
 import { NotifyEmailDto } from './dto/notify-email.dto';
 
 @Injectable()
 export class NotificationsService {
+  private readonly transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: this.configService.get('SMTP_USER'),
+      clientId: this.configService.get('GOOGLE_OAUTH_CLIENT_ID'),
+      clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+      refreshToken: this.configService.get('GOOGLE_OAUTH_REFRESH_TOKEN'),
+    },
+  });
+
+  constructor(private readonly configService: ConfigService) {}
+
   async notifyEmail(payload: NotifyEmailDto) {
     const { email } = payload;
     console.log(email);
+
+    const mailRes = await this.transporter.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'Sleepr Notification',
+      text: 'Test text',
+    });
+
+    console.log(mailRes);
   }
 }
